8fb0ce2e178944847d63cd62e739b45f34020a59
abc183801911 <570117624@qq.com>
2017-09-13 21:56:28 +0800

    创建springBoot项目

---
+++ springBoot/src/main/java/com/neo/springBoot/controller/JsonController.java
@@ -0,0 +1,20 @@
+package com.neo.springBoot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neo.springBoot.domain.User;
+
+@RestController
+public class JsonController {
+	@RequestMapping("/getUser")
+	public User getUser() {
+		User user = new User();
+		user.setUsername("小明");
+		user.setPassword("xxxx");
+		System.out.println("hello 小明");
+		return user;
+	}
+
+}
---
+++ springBoot/src/main/java/com/neo/springBoot/domain/User.java
@@ -0,0 +1,24 @@
+package com.neo.springBoot.domain;
+
+public class User {
+	private String Username;
+	private String Password;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	
+}
